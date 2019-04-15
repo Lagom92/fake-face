9c228521a973286371e1b0e61c845883f5598991
Lagom <jyyt0147@gmail.com>
2019-04-15 09:22:57 +0000

    list page

--- fake_face/settings.py
+++ fake_face/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7ewe#u$fz90*dky7nv50f@t#a@2+kygg!_c(1wo^%oh(xz8d8c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'fake_face.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

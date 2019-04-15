f5782fe168ccf6b02d885dd5bb158b1d3680bb7b
Lagom <jyyt0147@gmail.com>
2019-04-15 09:40:19 +0000

    create

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from .forms import PostForm
 # Create your views here.
 
 def list(request):
     return render(request, 'posts/list.html')
+    
+def create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = PostForm()
+    return render(request, 'posts/form.html', {'form':form})
